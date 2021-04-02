fb1c3952296def74a4e94269f06e6e542bbd95b1
Dong Liang <DongL@users.noreply.github.com>
2021-04-02 11:32:24 -0400

    Update Rejection_sampling.py

--- Rejection_sampling.py
+++ Rejection_sampling.py
@@ -8,31 +8,28 @@ from scipy.stats import norm, uniform
 import seaborn as sns 
 
 # Proposal distribution
-Q = norm(50, 30)
+Q = norm(50, 35)
 # Q = uniform(-50, 200)
 
 
 # Target distribution
-P = lambda x: norm.pdf(x, loc=30, scale=10) + norm.pdf(x, loc=80, scale=20)
+P = lambda x: norm.pdf(x, loc=20, scale=15) + norm.pdf(x, loc=90, scale=25)
 
 # Scaling factor
-x = np.arange(-50, 151)
+x = np.arange(-150, 150)
 k = max(P(x) / Q.pdf(x))
 
 # Initialization
-i = 0
-N = 5000
+N = 10000
 sample = list()
 U = uniform(0, 1)
 
 # Rejection sampling
-for _ in range(5000):
+for _ in range(N):
     x = Q.rvs()
     u = U.rvs() # * k * Q.pdf(x)
-    
     if u < P(x) / (k * Q.pdf(x)):
         sample.append(x)
-    i += 1
 
- # Plotting 
+# Plotting 
 sns.distplot(sample)
